1403ce788a6d965ff64f845f42168a5d2ea65832
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-09-09 23:13:05 +0800

    chore: add usage of persisted state for pinia

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,21 @@ import '@/styles/var.less'
 import '@/styles/mixin.less'
 import '@/styles/global.less'
 
+// 创建 Pinia 实例
+const pinia = createPinia()
+
+/**
+ * Pinia 支持功能扩展，例如本地持久化功能
+ *
+ * 在命令行运行 `npm i pinia-plugin-persistedstate` 安装持久化插件
+ * 在本文件里导入 `import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'`
+ * 取消注释下方的这行 `pinia.use` 代码启用插件，并根据文档对单个 Store 启用配置
+ *
+ * @see https://prazdevs.github.io/pinia-plugin-persistedstate/zh/guide/
+ */
+// pinia.use(piniaPluginPersistedstate)
+
 createApp(App)
-  .use(createPinia()) // 启用 Pinia
+  .use(pinia) // 启用 Pinia
   .use(router)
   .mount('#app')
