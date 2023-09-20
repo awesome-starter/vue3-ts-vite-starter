e7384c8cc70002a9e31d42f2019f6847f41f5429
chengpeiquan <chengpeiquan@teiron-inc.cn>
2023-09-20 13:23:25 +0800

    feat(uno): add some css var colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,16 @@
 import { defineConfig } from '@bassist/uno'
 
 // https://github.com/chengpeiquan/bassist/tree/main/packages/uno
-export default defineConfig()
+export default defineConfig({
+  theme: {
+    colors: {
+      brand: 'var(--c-brand)',
+      bg: 'var(--c-bg)',
+      text: 'var(--c-text)',
+      tips: 'var(--c-tips)',
+      border: 'var(--c-border)',
+      mask: 'var(--c-mask)',
+      hover: 'var(--c-hover)',
+    },
+  },
+})
