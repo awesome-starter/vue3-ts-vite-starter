3acc3b66b000109a91a912c7da66508402c7fb48
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-10-05 23:00:01 +0800

    fix: typescript detection problem

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior: (to, from, savedPosition) => {
+    console.info('to:', to, 'from', from)
     return savedPosition ? savedPosition : { top: 0, left: 0 }
   },
 })
